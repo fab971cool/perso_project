f88e6827cf8cd524d72a91b0d887b1a0295872b2
fab971cool <78422154+fab971cool@users.noreply.github.com>
2021-04-28 13:18:26 +0200

    fonction admin_add qt vue

--- main.py
+++ main.py
@@ -3,12 +3,17 @@ import sys
 from model.database import DatabaseEngine
 from PySide6.QtWidgets import QApplication
 from vue.login import LoginWindow
-
+from vue.user.add import AddUserQt
+from controller.admin import adminController
 
 if __name__ == "__main__":
     databaseEngine = DatabaseEngine(url='sqlite:///pathesiea.db')
     databaseEngine.create_database()
     app = QApplication(sys.argv)
+    print("test")
+    admin = adminController(databaseEngine)
+    test = AddUserQt()
+
     login = LoginWindow(databaseEngine)
     app.exec_()
 
